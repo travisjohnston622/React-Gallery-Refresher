cf54e128c31a3b146c6642938c31c636c81c148d
Travis Johnston <travisjohnston4588@icloud.com>
2020-07-20 14:21:14 -0500

    conditional rendering implemented in GalleryItem

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component {
+	state = {
+		showImage: true,
+	};
+
+	toggleImage = (event) => {
+		this.setState({
+			showImage: !this.state.showImage,
+		});
+	};
 	render() {
-		return (
-			<div>
-				<img src={this.props.item.path} alt={this.props.item.discription} />
-			</div>
+		let imageElement = (
+			<img src={this.props.item.path} alt={this.props.item.discription} />
 		);
+
+		if (!this.state.showImage) {
+			imageElement = <p>{this.props.item.discription}</p>;
+		}
+		return <div onClick={this.showImage}>{imageElement}</div>;
 	}
 }
 
