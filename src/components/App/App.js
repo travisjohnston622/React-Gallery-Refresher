79f0e01038a1fc6d9f4c92e503e482311f8b6b09
Travis Johnston <travisjohnston4588@icloud.com>
2020-07-20 13:20:38 -0500

    GET call created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of my life</h1>
-        </header>
-        <br/>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
-  }
+	componentDidMount() {
+		this.getGallery();
+	}
+	getGallery() {
+		Axios({
+			method: 'GET',
+			url: '/gallery',
+		})
+			.then((repsonse) => {
+				console.log(repsonse);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<h1 className='App-title'>Gallery of my life</h1>
+				</header>
+				<br />
+				<p>Gallery goes here</p>
+				<img src='images/goat_small.jpg' />
+			</div>
+		);
+	}
 }
 
 export default App;
